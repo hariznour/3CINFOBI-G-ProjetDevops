86ab545d119f2b1b52db3a4bc145ea6e4e84c1df
hariz-nour <work.nourhariz@gmail.com>
2025-01-04 00:19:45 +0100

    fix

--- src/test/java/tn/esprit/tpfoyer/service/EtudiantServiceImplTest.java
+++ src/test/java/tn/esprit/tpfoyer/service/EtudiantServiceImplTest.java
@@ -2,6 +2,7 @@ package tn.esprit.tpfoyer.service;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -17,6 +18,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class EtudiantServiceImplTest {
 
     @Autowired
+    @Mock
     private EtudiantServiceImpl etudiantService;
 
     @Test
